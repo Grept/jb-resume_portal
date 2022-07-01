2ca16382ddaff6c6de18b1da0d0f6e3f2e5f080f
Grept <tomjansen.408@gmail.com>
2022-07-01 10:18:05 +0200

    Added job entity and populated a userProfile with data.

--- src/main/java/com/grept/resumeportal/controllers/HomeController.java
+++ src/main/java/com/grept/resumeportal/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.grept.resumeportal.controllers;
 
+import com.grept.resumeportal.models.Job;
 import com.grept.resumeportal.models.UserProfile;
 import com.grept.resumeportal.repositories.UserProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.swing.text.html.Option;
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Optional;
 
 @Controller
@@ -21,7 +24,33 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello";
+        UserProfile profile1 = new UserProfile();
+        profile1.setId(1);
+        profile1.setDesignation("Current Designation");
+        profile1.setUserName("tom");
+        profile1.setFirstName("Tom");
+        profile1.setLastName("Jansen");
+        profile1.setTheme(1);
+
+        Job job1 = new Job();
+        job1.setCompany("Company 1");
+        job1.setDesignation("Designation 1");
+        job1.setId(3);
+        job1.setStartDate(LocalDate.of(2019, 5, 1));
+        job1.setEndDate(LocalDate.of(2020, 8, 1));
+
+        Job job2 = new Job();
+        job2.setCompany("Company 2");
+        job2.setDesignation("Designation 2");
+        job2.setId(4);
+        job2.setStartDate(LocalDate.of(2020, 9, 1));
+        job2.setEndDate(LocalDate.of(2021, 4, 1));
+
+        profile1.setJobs(Arrays.asList(job1, job2));
+
+        userProfileRepository.save(profile1);
+
+        return "profile";
     }
 
     @GetMapping("/edit")
@@ -37,6 +66,7 @@ public class HomeController {
         model.addAttribute("userId", userId);
         UserProfile userProfile = userProfileOptional.get();
         model.addAttribute("userProfile", userProfile);
+        System.out.println(userProfile.getJobs());
 
         return "profile-templates/" + userProfile.getTheme() + "/index";
     }
--- src/main/java/com/grept/resumeportal/models/UserProfile.java
+++ src/main/java/com/grept/resumeportal/models/UserProfile.java
@@ -1,6 +1,8 @@
 package com.grept.resumeportal.models;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table
@@ -12,6 +14,15 @@ public class UserProfile {
     private String userName;
     private int theme;
     private String summary;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String designation;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "job_id")
+    private List<Job> jobs = new ArrayList<>();
 
     public int getId() {
         return id;
@@ -29,7 +40,6 @@ public class UserProfile {
         this.userName = userName;
     }
 
-
     public int getTheme() {
         return theme;
     }
@@ -44,5 +54,53 @@ public class UserProfile {
 
     public void setSummary(String summary) {
         this.summary = summary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
     }
 }
